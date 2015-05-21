6935fe25d9b99049b924cec7118227e70a1b01d3
therealvdeadline <therealvdeadline@gmail.com>
2015-05-21 17:03:48 +1000

    Finished workspace switcher

--- .config/lemonbar/bar.sh
+++ .config/lemonbar/bar.sh
@@ -70,7 +70,7 @@ workspace(){
 	workspaceprevious="A5:i3-msg workspace prev_on_output:"
 
 	# Prints a list of all open workspaces and highlights the active workspace, the empty double quotes are needed for formatting
-	wslist=$(wmctrl -d | awk '/ / {printf $2 $9 $10 $11 $12 $13 $14 $15}' | sed -e 's/\*[0-9*.*(?s:.)*.*[A-Za-z]*]*/%{B#AFC4DB} & %{B}/g' -e 's/\-[0-9*.*(?s:.)*.*[A-Za-z]*]*/%{B#7587A6}%{A:i3-msg workspace &:} & %{A}%{B}/g' -e 's/\*//g' -e 's/\ -/ /g')
+	wslist=$(wmctrl -d | awk '/ / {print $2 $9, $10, $11, $12}' ORS='' | sed -e 's/  //g' -e 's/\*[0-9 A-Za-z]*[^ -~]*/%{B#AFC4DB}  & %{B}/g' -e 's/\-[0-9 A-Za-z]*[^ -~]*/%{B#7587A6}%{A:i3-msg workspace &:}  & %{A}%{B}/g' -e 's/\*//g' -e 's/\ -/ /g')
 
 	# Space infront of $wslist is needed to center the output.
 	echo "%{$workspacenext}%{$workspaceprevious}$wslist%{A}%{A}"
