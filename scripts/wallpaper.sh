3fc10209dd5862225d094eb2fb3865930682dd7a
dylanaraps <dyl@tfwno.gf>
2015-10-11 16:54:26 +1100

    ddd

--- scripts/wallpaper.sh
+++ scripts/wallpaper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while :; do
-	find ~/Pictures/papes -type f \( -name '*.jpg' -o -name '*.png' \) -print0 |
+	find ~/Pictures/papes2 -type f \( -name '*.jpg' -o -name '*.png' \) -print0 |
     shuf -n1 -z | xargs -0 feh --bg-fill
     sleep 15m
 done
