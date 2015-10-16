648ad6d8eb909cf5c065a599e11af15f5fa36199
dylanaraps <dyl@tfwno.gf>
2015-10-16 11:42:04 +1100

    support for external 4k tv

--- .config/lemonbar/workspace.sh
+++ .config/lemonbar/workspace.sh
@@ -9,16 +9,16 @@ workspace(){
 	query=$(bspc query -D -d)
 
 	case $query in
-		$ws1 )
-			echo "%{B#B4BCC9}%{F#757B8E}  $ws1  %{F}%{B}%{B#757B8E}%{A:bspc desktop -f $ws2:}  $ws2  %{A}%{B}%{B#757B8E}%{A:bspc desktop -f $ws3:}  $ws3  %{A}%{B}%{B#757B8E}%{A:bspc desktop -f $ws4:}  $ws4  %{A}%{B}" ;;
-		$ws2 )
-			echo "%{B#757B8E}%{A:bspc desktop -f $ws1:}  $ws1  %{A}%{B}%{B#B4BCC9}%{F#757B8E}  $ws2  %{F}%{B}%{B#757B8E}%{A:bspc desktop -f $ws3:}  $ws3  %{A}%{B}%{B#757B8E}%{A:bspc desktop -f $ws4:}  $ws4  %{A}%{B}" ;;
-		$ws3 )
-			echo "%{B#757B8E}%{A:bspc desktop -f $ws1:}  $ws1  %{A}%{B}%{B#757B8E}%{A:bspc desktop -f $ws2:}  $ws2  %{A}%{B}%{B#B4BCC9}%{F#757B8E}  $ws3  %{F}%{B}%{B#757B8E}%{A:bspc desktop -f $ws4:}  $ws4  %{A}%{B}" ;;
-		$ws4 )
-			echo "%{B#757B8E}%{A:bspc desktop -f $ws1:}  $ws1  %{A}%{B}%{B#757B8E}%{A:bspc desktop -f $ws2:}  $ws2  %{A}%{B}%{B#757B8E}%{A:bspc desktop -f $ws3:}  $ws3  %{A}%{B}%{B#B4BCC9}%{F#757B8E}  $ws4  %{F}%{B}" ;;
+		1 )
+			echo "%{B#B4BCC9}%{F#757B8E}  $ws1  %{F}%{B}%{B#757B8E}%{A:bspc desktop -f 2:}  $ws2  %{A}%{B}%{B#757B8E}%{A:bspc desktop -f 3:}  $ws3  %{A}%{B}%{B#757B8E}%{A:bspc desktop -f 4:}  $ws4  %{A}%{B}" ;;
+		2 )
+			echo "%{B#757B8E}%{A:bspc desktop -f 1:}  $ws1  %{A}%{B}%{B#B4BCC9}%{F#757B8E}  $ws2  %{F}%{B}%{B#757B8E}%{A:bspc desktop -f 3:}  $ws3  %{A}%{B}%{B#757B8E}%{A:bspc desktop -f 4:}  $ws4  %{A}%{B}" ;;
+		3 )
+			echo "%{B#757B8E}%{A:bspc desktop -f 1:}  $ws1  %{A}%{B}%{B#757B8E}%{A:bspc desktop -f 2:}  $ws2  %{A}%{B}%{B#B4BCC9}%{F#757B8E}  $ws3  %{F}%{B}%{B#757B8E}%{A:bspc desktop -f 4:}  $ws4  %{A}%{B}" ;;
+		4 )
+			echo "%{B#757B8E}%{A:bspc desktop -f 1:}  $ws1  %{A}%{B}%{B#757B8E}%{A:bspc desktop -f 2:}  $ws2  %{A}%{B}%{B#757B8E}%{A:bspc desktop -f 3:}  $ws3  %{A}%{B}%{B#B4BCC9}%{F#757B8E}  $ws4  %{F}%{B}" ;;
 		* )
-			echo "%{B#757B8E}%{A:bspc desktop -f $ws1:}  $ws1  %{A}%{B}%{B#757B8E}%{A:bspc desktop -f $ws2:}  $ws2  %{A}%{B}%{B#757B8E}%{A:bspc desktop -f $ws3:}  $ws3  %{A}%{B}%{B#757B8E}%{A:bspc desktop -f $ws4:}  $ws4  %{A}%{B}" ;;
+			echo "%{B#757B8E}%{A:bspc desktop -f 1:}  $ws1  %{A}%{B}%{B#757B8E}%{A:bspc desktop -f 2:}  $ws2  %{A}%{B}%{B#757B8E}%{A:bspc desktop -f 3:}  $ws3  %{A}%{B}%{B#757B8E}%{A:bspc desktop -f 4:}  $ws4  %{A}%{B}" ;;
 	esac
 }
 
