92ec54d80171c88401603d5a96dac78d47f49334
dylanaraps <dyl@tfwno.gf>
2015-10-22 19:33:43 +1100

    wewow

--- scripts/wallpaper.sh
+++ scripts/wallpaper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while :; do
-	find ~/Pictures/papes3 -type f \( -name '*.jpg' -o -name '*.png' \) -print0 |
+	find ~/Pictures/papes4 -type f \( -name '*.jpg' -o -name '*.png' \) -print0 |
     shuf -n1 -z | xargs -0 feh --bg-fill
     sleep 15m
 done
