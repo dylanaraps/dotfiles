b61d213a54661b2af8be511ad51659f94fcf039b
dylanaraps <dyl@tfwno.gf>
2015-11-06 09:53:00 +1100

    New window center script that works with all windows

--- scripts/wmutils/centerwin.sh
+++ scripts/wmutils/centerwin.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 # get current window id, width and height
-WID=$(pfw)
-WW=$(wattr w $WID)
-WH=$(wattr h $WID)
+WID=$(xprop -root '_NET_ACTIVE_WINDOW' | grep --only-matching '0x.*')
+WW=$(xwininfo -id $WID | awk '/Width:/ {print $2}')
+WH=$(xwininfo -id $WID | awk '/Height:/ {print $2}')
 SW=1920
 SH=1080
 
 # move the current window to the center of the screen
-wtp $(((SW - WW)/2)) $(((SH - WH)/2)) $WW $WH $WID
+# wtp $(((SW - WW)/2)) $(((SH - WH)/2)) $WW $WH $WID
+wmctrl -r :ACTIVE: -e 2,$(((SW - WW)/2)),$(((SH - WH)/2)),$WW,$WH
+
