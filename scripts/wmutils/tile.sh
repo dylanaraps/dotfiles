f109847057585f7e52cc10c47106df072cc32ae1
dylan araps <dyl@tfwno.gf>
2015-11-24 21:43:48 +1100

    update tile script

--- scripts/wmutils/tile.sh
+++ scripts/wmutils/tile.sh
@@ -21,8 +21,8 @@ MAX=$(lsw | grep -v $PFW | wc -l)
 IGNORE=$(wname $(lsw) | grep "tile_ignore" | wc -l)
 
 # calculate usable screen size (without borders and gaps)
-SW=$((1920 - GAP - 2*BW))
-SH=$((1080 - GAP - 2*BW - PANEL))
+SW=$((1920 - $GAP - 2*$BW))
+SH=$((1080 - $GAP - 2*$BW - $PANEL))
 
 Y=$(($GAP + $PANEL))
 
@@ -30,27 +30,30 @@ Y=$(($GAP + $PANEL))
 
 # If there's only one unignored window open tile it to the full width of the screen
 if [[ $((`lsw | wc -l` - $IGNORE)) == 1 ]]; then
-    ~/dotfiles/scripts/wmutils/maximize.sh $GAP $GAP
+    SW=$(($SW - $GAP))
+    SH=$(($SH - $GAP))
+    wtp $GAP $GAP $SW $SH $PFW
+
 # Prevent tiling of ignored windows
-elif [[ $(wname `pfw`) == *"tile_ignore"* ]]; then
+elif [[ $(wname $PFW) == *"tile_ignore"* ]]; then
     exit
 else
-    wtp $GAP $Y $((MASTER - GAP - 2*BW)) $((SH - GAP)) $PFW
+    wtp $GAP $Y $(($MASTER - $GAP - 2*$BW)) $(($SH - $GAP)) $PFW
 fi
 
 # Put the tiled windows at the bottom of the stack
 chwso $(pfw) -l
 
 # and now, stack up all remaining windows on the right
-X=$((MASTER + GAP))
-W=$((SW - MASTER - GAP))
-H=$((SH / $(($MAX - $IGNORE)) - GAP))
+X=$(($MASTER + $GAP))
+W=$(($SW - $MASTER - $GAP))
+H=$(($SH / $(($MAX - $IGNORE)) - $GAP))
 
 for wid in $(lsw | grep -v $PFW); do
     # If focused window's name doesn't include "tile_ignore", tile it!
     if [[ $(wname $wid) != *"tile_ignore"* ]]; then
         wtp $X $Y $W $H $wid
-        Y=$((Y + H + GAP))
+        Y=$(($Y + $H + $GAP))
 
         # Put the tiled windows at the bottom of the stack
         chwso $wid -l
