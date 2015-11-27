6fee2835afb91fa4286f5e0f9dd3e818d795e9e5
dylan araps <dyl@tfwno.gf>
2015-11-28 10:30:30 +1100

    colors

--- scripts/colors.sh
+++ scripts/colors.sh
@@ -3,15 +3,12 @@
 # Prints terminal colors with hex colors side by side
 # Only works with terminals that use .Xdefaults/.Xresources
 
-number="-1"
-
-echo ""
+number="0"
 
 # This echo is outside of the for loop to manually set it's text color
-for color in $( xrdb -query | awk '/*.color[0-8]:/ {print $2}' ); do
-    number=$((number + 1))
-    echo -n "$(tput setaf "$number")$(tput smso)       $(tput sgr0)"
-done
+echo "$(tput setaf 0)$(tput smso)       $(tput sgr0)$(tput setaf 8) $( xrdb -query | awk '/*.color0:/ {print $2}' )"
 
-echo ""
-echo ""
+for color in $( xrdb -query | awk '/*.color[1-8]:/ {print $2}' ); do
+    number=$((number + 1))
+    echo "$(tput setaf "$number")$(tput smso)       $(tput sgr0)$(tput setaf "$number") $color"
+done
