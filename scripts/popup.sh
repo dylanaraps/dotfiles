222fb5a97fd50fad142f92fcc74c27fff2220ccc
dylan araps <dyl@tfwno.gf>
2015-11-28 15:32:51 +1100

    Update popup.sh

--- scripts/popup.sh
+++ scripts/popup.sh
@@ -1,16 +1,41 @@
 #!/bin/sh
 # Notifications using lemonbar
 
-# USAGE:
-# $1 gets displayed
-# $2 sleep duration
-# $3 notification width
-# $4 xoffset
+# Variables
+
+barname="popups"
+sleep=2
+
+width=150
+height=30
+xoffset=1800
+yoffset=35
+
+bg="#$white"
+fg="#$black"
+
+font="drift"
+
+# Options
+
+while getopts "w:h:x:y:bg:fg:n:s:f:e:" opt; do
+    case $opt in
+        w) width="$OPTARG" ;;
+        h) height="$OPTARG" ;;
+        x) xoffset="$OPTARG" ;;
+        y) yoffset="$OPTARG" ;;
+        bg) bg="$OPTARG" ;;
+        fg) fg="$OPTARG" ;;
+        n) barname="$OPTARG" ;;
+        s) sleep="$OPTARG" ;;
+        f) font="$OPTARG" ;;
+        e) echo="$OPTARG" ;;
+    esac
+done
 
 # Kill previous notifications
-ps -ef | awk '/popups/ {print $2}' | xargs kill
+ps -ef | awk -v name="$barname" '$0 ~ name {print $2}' | xargs kill 2>/dev/null
 
 # Start notification
-(echo "%{c} $1 %{c}"; sleep $2) | lemonbar -n "popups" -d -g "$3x30+$4+35" -f "drift" -B "#$white" -F "#$black"
-
+(echo "%{c} $echo %{c}"; sleep $sleep) | lemonbar -n "$barname" -d -g "$width"x"$height+$xoffset+$yoffset" -f "$font" -B "$bg" -F "$fg"
 
