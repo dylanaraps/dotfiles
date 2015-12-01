ee81bda24cb7d56b8adc6a6436e489ae502efc7b
dylan araps <dyl@tfwno.gf>
2015-12-01 23:29:25 +1100

    corner.sh: multimon support, also middle{left, right}

--- scripts/wmutils/corner.sh
+++ scripts/wmutils/corner.sh
@@ -3,31 +3,78 @@
 # z3bra - 2014 (c) wtfpl
 # move windows to the corners
 
+# Focused window
 wid=$(pfw)
 
-# Hardcoded w/h due to multimon
-sw=1900
-sh=1073
+# Set gap around corners
+gap=80
 
-# Get border/window size
+# Border width
 bw=$(wattr b $wid)
+
+# Window size
 w=$(wattr w $wid)
 h=$(wattr h $wid)
 
-# Set gap around corner
-gap=80
-
-# Set gap for x/y
+# Default x/y values
 x=$gap
 y=$gap
 
-case $1 in
-    tr) x=$((sw - w - bw*2 - $gap)) ;;
-    bl) y=$((sh - h - bw*2 - $gap)) ;;
-    br) x=$((1900 - w - bw*2 - $gap))
-        y=$((1073 - h - bw*2 - $gap)) ;;
-    md) x=$((sw/2 - w/2 - bw))
-        y=$((sh/2 - h/2 - bw)) ;;
-esac
+# Hardvoded multimonitor
+if [[ $(wattr y $wid) -gt 1080 ]]; then
+    case $1 in
+        tl) y=$((1080 + gap)) ;;
 
+        tr) x=$((1920 - gap - w - bw*2))
+            y=$((1080 + gap)) ;;
+
+        bl) y=$((1080*2 - gap - h - bw*2)) ;;
+
+        br) x=$((1920 - gap - w - bw*2))
+            y=$((2160 - gap - h - bw*2)) ;;
+
+        ml) y=$((1080 + 1080/2 - h/2 - bw)) ;;
+
+        mr) x=$((1920 - gap - w - bw*2))
+            y=$((1080 + 1080/2 - h/2 - bw)) ;;
+    esac
+elif [[ $(wattr x $wid) -gt 1920 ]]; then
+    case $1 in
+        tl) x=$((1920 + gap)) ;;
+
+        tr) x=$((1920 + 1280 - gap - w - bw*2)) ;;
+
+        bl) x=$((1920 + gap))
+            y=$((1024 - gap - h - bw*2)) ;;
+
+        br) x=$((1920 + 1280 - gap - w - bw*2))
+            y=$((1024 - gap - h - bw*2)) ;;
+
+        ml) x=$((1920 + gap))
+            y=$((1024/2 - h/2 - bw)) ;;
+
+        mr) x=$((1920 + 1280 - gap - w - bw*2))
+            y=$((1024/2 - h/2 - bw)) ;;
+    esac
+else
+    case $1 in
+        # Keeping this here for readability
+        tl) x=$gap
+            y=$gap ;;
+
+        tr) x=$((1920 - gap - w - bw*2)) ;;
+
+        bl) y=$((1080 - gap - h - bw*2)) ;;
+
+        br) x=$((1920 - gap - w - bw*2))
+            y=$((1080 - gap - h - bw*2)) ;;
+
+        ml) y=$((1080/2 - h/2 - bw)) ;;
+
+        mr) x=$((1920 - gap - w - bw*2))
+            y=$((1080/2 - h/2 - bw)) ;;
+    esac
+fi
+
+# Move the window
 wtp $x $y $w $h $wid
