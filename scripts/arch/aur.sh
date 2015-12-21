ea61dd18cc05e55c0d59e4161f95564011300d45
dylan araps <dyl@tfwno.gf>
2015-12-21 16:11:19 +1100

    Removed ignore option as I no longer need it

--- scripts/arch/aur.sh
+++ scripts/arch/aur.sh
@@ -46,25 +46,13 @@ uninstall () {
     done
 }
 
-# Update all aur packages
+# Update all aur packages in folder
 update_all () {
     cd "$aurdir" || exit
 
-    # List all packages and sort them alphabetically
-    packages=($(find . -mindepth 1 -maxdepth 1 -type d | sort))
-
-    # Packages to ignore.
-    ignore=(linux-libre-pck ttf-ms-fonts)
-
-    # Remove ignored packages from packages array
-    for pkg in ${ignore[@]}; do
-        packages=(${packages[@]/$pkg})
-    done
-
-    # Update unignored the packages
-    for pkg in ${packages[@]}; do
+    for pkg in *; do
         # cd into the package's directory and run makepkg
-        cd "$(basename $pkg)" || exit
+        cd "$pkg" || exit
         makepkg $mkflags
         cd "$aurdir"
     done
