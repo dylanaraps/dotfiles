0d26182cc16be7e66253c7195a9eab7db1652512
Dylan Araps <dylan.araps@gmail.com>
2017-12-20 11:02:51 +1100

    update

--- install.sh
+++ install.sh
@@ -4,11 +4,12 @@
 
 q() {
     read -r -n 1 -p "$1 [y/n] " response
-    printf "\n" 
-    [[ "${response,,}" == "y" ]] && local result=0  
+    printf "\n"
+    [[ "${response,,}" == "y" ]] && local result=0
 }
 
-q "Install dotfiles for home?" && stow home
-q "Install dotfiles for openbox?" && stow openbox 
-q "Install dotfiles for (n)vim/?" && stow vim 
+q "Install dotfiles for home?"    && stow home
+q "Install dotfiles for openbox?" && stow openbox
+q "Install dotfiles for (n)vim?"  && stow vim
+q "Install dotfiles for mpv?"     && stow mpv
 q "Install dotfiles for system (requires sudo)?" && sudo stow system -t /
